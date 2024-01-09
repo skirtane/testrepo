d5842fc8202800c378b97611ec93ad807ee779c5
Suraj Kirtane <suraj.kirtane@example.com>
2024-01-09 01:58:52 -0500

    Fixes linter based issues

--- firstpython.py
+++ firstpython.py
@@ -1,13 +1,15 @@
 # Add print statement
 print("New python file")
-'''
+"""
 for x in range(7):
   print(x)
-'''
+"""
+
 
 def greetUser(name):
-    print('Hello ' +name)
+    print("Hello " + name)
+
 
 if __name__ == "__main__":
-  #print('This is main invocation!')
-  greetUser('Suraj')
+    # print('This is main invocation!')
+    greetUser("Suraj")
