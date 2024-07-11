c3534f5435b746a0bb86ea3e3865850a096c1deb
Monali Selsurkar <mselsurkar@remotedev-mselsurkar-hop.west.isilon.com>
2024-07-11 07:23:48 +0000

    This is the 1st commit

---
+++ myname.py
@@ -0,0 +1,9 @@
+print("my first code")
+
+def myname(name):
+    # this will print the name
+    print("My name is "+ name)
+    
+if __name__ == "__main__":
+    # main invocation
+    myname("Monali")
